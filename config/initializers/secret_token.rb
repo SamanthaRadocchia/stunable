78d64917cd60729a9797788f9cad9716fb917c42
SamanthaRadocchia <samantha.radocchia@gmail.com>
2013-07-08 12:22:48 -0400

    second attempt after failed app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Stunable::Application.config.secret_token = 'd9bb854a6b12685d0d20857a5065c7219cfc216dfe365de049ea5ac74453f1683a27ca9f39c39248bae6172317cc3f6fdeafa13d82a1ccc40ade0cca81b79e25'
